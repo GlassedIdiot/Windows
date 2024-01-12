015c5c92c6a571c73754dc43d6205a4d4be37c4d
GlassedIdiot <rehmanareeb69@gmail.com>
2024-01-12 17:00:11 +0500

    Updated Create-Process and Hellworld.c

--- Create-Process.c
+++ Create-Process.c
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 #include <stdio.h>
 #include <windows.h>
 
@@ -27,62 +25,4 @@ int main(void)
     }
 
     return EXIT_SUCCESS;
-=======
-#include <stdio.h>
-#include <windows.h>
-
-int main(void)
-{
-
-    STARTUPINFOW startup_info = {0};
-
-    PROCESS_INFORMATION process_info = {0};
-
-    if (!CreateProcessW(
-            L"C:\\Windows\\System32\\notepad.exe",
-            NULL,
-            NULL,
-            NULL,
-            FALSE,
-            BELOW_NORMAL_PRIORITY_CLASS,
-            NULL,
-            NULL,
-            &startup_info,
-            &process_info))
-    {
-        printf("(-)Failed to create process, error: %ld", GetLastError());
-        return EXIT_FAILURE;
-    }
-
-    return EXIT_SUCCESS;
->>>>>>> 742305cb6126e51427c0cfc5919f67e3c8696942
-=======
-#include <stdio.h>
-#include <windows.h>
-
-int main(void)
-{
-
-    STARTUPINFOW startup_info = {0};
-
-    PROCESS_INFORMATION process_info = {0};
-
-    if (!CreateProcessW(
-            L"C:\\Windows\\System32\\notepad.exe",
-            NULL,
-            NULL,
-            NULL,
-            FALSE,
-            BELOW_NORMAL_PRIORITY_CLASS,
-            NULL,
-            NULL,
-            &startup_info,
-            &process_info))
-    {
-        printf("(-)Failed to create process, error: %ld", GetLastError());
-        return EXIT_FAILURE;
-    }
-
-    return EXIT_SUCCESS;
->>>>>>> 742305cb6126e51427c0cfc5919f67e3c8696942
 }
--- HelloWorld.c
+++ HelloWorld.c
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 #include <stdio.h>
 #include <Windows.h>
 
@@ -12,32 +10,4 @@ int main(void)
       "Welcome",
       MB_OKCANCEL | MB_ICONASTERISK);
   return EXIT_SUCCESS;
-=======
-#include <stdio.h>
-#include <Windows.h>
-
-int main(void)
-{
-  MessageBox(
-
-      NULL,
-      "Hello. Press OK if you are happy with life OR cancel to end it.",
-      "Welcome",
-      MB_OKCANCEL | MB_ICONASTERISK);
-  return EXIT_SUCCESS;
->>>>>>> 742305cb6126e51427c0cfc5919f67e3c8696942
-=======
-#include <stdio.h>
-#include <Windows.h>
-
-int main(void)
-{
-  MessageBox(
-
-      NULL,
-      "Hello. Press OK if you are happy with life OR cancel to end it.",
-      "Welcome",
-      MB_OKCANCEL | MB_ICONASTERISK);
-  return EXIT_SUCCESS;
->>>>>>> 742305cb6126e51427c0cfc5919f67e3c8696942
 }
