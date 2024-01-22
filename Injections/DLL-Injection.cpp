777e67c3cff49963876c1f09791798ea817f4131
GlassedIdiot <rehmanareeb69@gmail.com>
2024-01-22 22:46:14 +0500

    .

--- Injections/DLL-Injection.cpp
+++ Injections/DLL-Injection.cpp
@@ -1,6 +1,7 @@
-#include <windows.h>
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<windows.h>
+
+
 #define okay(msg, ...) printf("[+] " msg "\n", ##__VA_ARGS__)
 #define warn(msg, ...) printf("[-] " msg "\n", ##__VA_ARGS__)
 #define info(msg, ...) printf("[i]" msg "\n", ##__VA_ARGS__)
